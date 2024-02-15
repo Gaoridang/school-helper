9e00b4cd8a8a1db90adfef95ba49bf86a937cbd6
Gaoridang <oingc@naver.com>
2024-02-16 02:33:57 +0900

    Remove: 안 쓰는 아이콘 제거

--- app/check/TopicSelect.tsx
+++ app/check/TopicSelect.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Check, ChevronsUpDown, PlusIcon } from "lucide-react";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
