aab773f2edc5e111f958c45ee2e9d5c65ce3a5df
Gaoridang <oingc@naver.com>
2024-02-16 13:26:02 +0900

    Add: 활동 선택하기

---
+++ app/activities/_components/ActivitySelector.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { useState } from "react";
+import { AddSheet } from "./AddSheet";
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "@/app/utils/supabase/client";
+
+const ActivitySelector = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  const { data: activities } = useQuery({
+    queryKey: ["activities"],
+    queryFn: async () => {
+      const { data, error } = await supabase.from("activity").select("*");
+      console.log(data);
+      if (error) throw new Error(error.message);
+      return data;
+    },
+  });
+
+  return (
+    <div className="flex items-center gap-2">
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between"
+          >
+            {value ? activities?.find((item) => item.name === value)?.name : "활동"}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="역할극" />
+            <CommandEmpty>
+              <Button>추가하기</Button>
+            </CommandEmpty>
+            <CommandGroup>
+              {activities?.map((item) => (
+                <CommandItem
+                  key={item.name}
+                  value={item.name}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === item.name ? "opacity-100" : "opacity-0",
+                    )}
+                  />
+                  {item.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+      <AddSheet />
+    </div>
+  );
+};
+
+export default ActivitySelector;
---
+++ app/activities/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ActivitySelector from "./_components/ActivitySelector";
+
+const ActivityListPage = () => {
+  return <ActivitySelector />;
+};
+
+export default ActivityListPage;
