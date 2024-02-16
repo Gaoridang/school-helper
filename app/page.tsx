e1102780c695b0360852e2aaab5f89cedb04461b
Lee JaeJun <monooi@naver.com>
2024-02-16 13:26:40 +0900

    Add: 로그인 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import TopicSelect from "./check/TopicSelect";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import SignInButton from "./components/SignInButton";
 
 export default function Home() {
   return (
     <div className="max-w-xl m-auto">
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-5">
-        체크리스트 만들기
-      </h2>
-      <TopicSelect />
+      <Link href="/activities">
+        <Button>체크리스트 만들기</Button>
+      </Link>
+      <SignInButton />
     </div>
   );
 }
