f38a1dbf8b00244d87e2217367438d9ab8d529e3
Lee JaeJun <monooi@naver.com>
2024-02-17 01:34:52 +0900

    Fix: 최대 너비 제거

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import SignInButton from "./components/SignInButton";
 
 export default function Home() {
   return (
-    <div className="max-w-xl m-auto">
+    <>
       <Link href="/activities">
         <Button>체크리스트 만들기</Button>
       </Link>
       <SignInButton />
-    </div>
+    </>
   );
 }
