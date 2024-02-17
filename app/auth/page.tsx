6f03e2200f1eadfd017a9189bf626d4cdcd1c09f
Gaoridang <oingc@naver.com>
2024-02-17 20:41:45 +0900

    Add: 로그인 회원가입 페이지

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,27 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import SignInForm from "./_components/SignInForm";
+import SignUpForm from "./_components/SignUpForm";
 
 const AuthPage = () => {
-  const [email, setEmail] = useState("");
-  const sendConfirmEmail = async () => {
-    await axios.post(
-      "/api/send",
-      { email },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-    );
-  };
-
   return (
     <div className="flex justify-center items-center p-10">
       <Tabs defaultValue="signin" className="w-[400px]">
@@ -30,55 +14,10 @@ const AuthPage = () => {
           <TabsTrigger value="signup">회원가입</TabsTrigger>
         </TabsList>
         <TabsContent value="signin">
-          <Card>
-            <CardHeader>
-              <CardTitle>로그인</CardTitle>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">이메일</Label>
-                <Input
-                  id="name"
-                  placeholder="example@email.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Button onClick={sendConfirmEmail}>이메일 확인</Button>
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">비밀번호</Label>
-                <Input id="username" defaultValue="@peduarte" />
-              </div>
-            </CardContent>
-            <CardFooter className="flex space-x-2">
-              <Button>로그인</Button>
-              <Button variant="ghost">비밀번호 재설정</Button>
-            </CardFooter>
-          </Card>
+          <SignInForm />
         </TabsContent>
         <TabsContent value="signup">
-          <Card>
-            <CardHeader>
-              <CardTitle>회원가입</CardTitle>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">이메일</Label>
-                <Input id="name" defaultValue="Pedro Duarte" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">비밀번호</Label>
-                <Input id="username" defaultValue="@peduarte" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">비밀번호 확인</Label>
-                <Input id="username" defaultValue="@peduarte" />
-              </div>
-            </CardContent>
-            <CardFooter className="flex space-x-2">
-              <Button>회원가입</Button>
-            </CardFooter>
-          </Card>
+          <SignUpForm />
         </TabsContent>
       </Tabs>
     </div>
