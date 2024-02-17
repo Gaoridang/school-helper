9e3fb66de8da65f51eb13e5a317bffa31e4cc1ed
Gaoridang <oingc@naver.com>
2024-02-17 20:41:27 +0900

    Add: 비밀번호 유효성 패턴 추가

--- app/api/authSchema.ts
+++ app/api/authSchema.ts
@@ -1,10 +1,17 @@
 import { z } from "zod";
 
-export const authSchema = z
+export const signUpSchema = z
   .object({
     email: z.string().email({ message: "이메일 형식에 맞지 않습니다." }).min(1).max(30),
-    password: z.string().min(1, "비밀번호를 입력하세요.").max(15, "비밀번호는 최대 15자리 입니다."),
-    passwordConfirm: z.string().min(1).max(15),
+    password: z
+      .string()
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{10,}$/,
+        "영문 소문자, 대문자, 숫자를 조합해 10자리 이상 입력하세요",
+      )
+      .min(1, "비밀번호를 입력하세요.")
+      .max(15, "비밀번호는 최대 15자리 입니다."),
+    passwordConfirm: z.string(),
   })
   .refine((data) => data.password === data.passwordConfirm, {
     message: "비밀번호가 일치하지 않습니다.",
@@ -15,5 +22,5 @@ export const sendEmailSchema = z.object({
   email: z.string().email(),
 });
 
-export type AuthType = z.infer<typeof authSchema>;
+export type SignUpType = z.infer<typeof signUpSchema>;
 export type SendEmailType = z.infer<typeof sendEmailSchema>;
