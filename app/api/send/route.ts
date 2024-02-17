1c8c3d1e40ceed60602a39ca9eb95540d6328007
Gaoridang <oingc@naver.com>
2024-02-17 16:17:20 +0900

    Fix: 이메일 인증 코드 수정

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 import { sendEmailSchema } from "../authSchema";
+import { EmailTemplate } from "@/components/email-templates";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
@@ -10,14 +11,18 @@ export async function POST(request: NextRequest) {
   if (!validation.success) return NextResponse.json({ error: "Invalid type" }, { status: 400 });
 
   try {
-    const data = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: "gachiga@togethers.info",
-      to: [validation.data.email],
+      to: validation.data.email,
       subject: "Hello world",
-      html: "<string>It works</strong>",
+      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
     });
 
-    return Response.json(data);
+    if (error) {
+      return Response.json({ error });
+    }
+
+    return Response.json({ data });
   } catch (error) {
     return Response.json({ error });
   }
