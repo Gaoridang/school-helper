0fc9709b421880e51843a74ab56c33297012fda8
Gaoridang <oingc@naver.com>
2024-02-17 20:43:25 +0900

    Remove: 필요 없는 컴포넌트 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import SignInButton from "./components/SignInButton";
 
 export default function Home() {
   return (
@@ -8,7 +7,6 @@ export default function Home() {
       <Link href="/activities">
         <Button>체크리스트 만들기</Button>
       </Link>
-      <SignInButton />
     </>
   );
 }
