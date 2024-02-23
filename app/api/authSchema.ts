fb833c5ca1957fdcae42a2d63b092f6dad53b3a5
Gaoridang <oingc@naver.com>
2024-02-23 22:08:33 +0900

    Update: 회원가입 스키마 수정

--- app/api/authSchema.ts
+++ app/api/authSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const signUpSchema = z
+export const SignUpSchema = z
   .object({
     email: z.string().email({ message: "이메일 형식에 맞지 않습니다." }).min(1).max(30),
     password: z
@@ -12,6 +12,9 @@ export const signUpSchema = z
       .min(1, "비밀번호를 입력하세요.")
       .max(15, "비밀번호는 최대 15자리 입니다."),
     passwordConfirm: z.string(),
+    school: z.string().min(1, "학교를 선택하세요"),
+    grade: z.string().min(1, "학년을 선택하세요"),
+    classNumber: z.string().min(1, "반을 선택하세요."),
   })
   .refine((data) => data.password === data.passwordConfirm, {
     message: "비밀번호가 일치하지 않습니다.",
@@ -22,5 +25,5 @@ export const sendEmailSchema = z.object({
   email: z.string().email(),
 });
 
-export type SignUpType = z.infer<typeof signUpSchema>;
+export type SignUpType = z.infer<typeof SignUpSchema>;
 export type SendEmailType = z.infer<typeof sendEmailSchema>;
