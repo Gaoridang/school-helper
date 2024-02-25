e2e2a762f702408008ccd042504ffc74752901a4
Lee JaeJun <monooi@naver.com>
2024-02-25 23:32:09 +0900

    Add: 학생 회원가입

--- app/signup/SignUpStudent.tsx
+++ app/signup/SignUpStudent.tsx
@@ -1,21 +1,58 @@
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { createRandomCode } from "../classes/utils/getRandomCode";
+import { signUpStudent } from "./actions";
 
 const formValues = [
-  { label: "이름", value: "name", placeholder: "홍길동" },
+  { label: "영어 닉네임", value: "name", placeholder: "dog" },
   { label: "번호", value: "student_number", placeholder: "12" },
-  { label: "코드번호", value: "code", placeholder: "123456" },
-];
+  { label: "학급코드", value: "class_code", placeholder: "선생님께 받은 학급 코드를 입력하세요." },
+  { label: "고유번호", value: "code", placeholder: "고유번호를 입력하세요" },
+  { label: "비밀번호", value: "password", placeholder: "비밀번호를 입력하세요" },
+] as const;
+
+const SignUpStudentSchema = z.object({
+  name: z.string().min(1, "이름을 입력하세요."),
+  student_number: z.string().min(1, "번호를 입력하세요."),
+  password: z.string().min(1, "비밀번호를 입력하세요."),
+  code: z.string().min(1, "코드번호를 입력하세요."),
+  class_code: z.string().min(1, "학급코드를 입력하세요."),
+});
+
+export type SignUpStudentType = z.infer<typeof SignUpStudentSchema>;
 
 const SignUpStudent = () => {
-  const form = useForm();
+  const initialCode = createRandomCode();
+  const form = useForm<SignUpStudentType>({
+    resolver: zodResolver(SignUpStudentSchema),
+    defaultValues: {
+      name: "",
+      student_number: "",
+      class_code: "",
+      code: initialCode,
+      password: initialCode,
+    },
+  });
+
+  const onSubmit = (data: SignUpStudentType) => {
+    signUpStudent(data);
+  };
 
   return (
     <Form {...form}>
-      <form className="flex flex-col gap-2">
+      <form className="flex flex-col gap-2" onSubmit={form.handleSubmit(onSubmit)}>
         {formValues.map((value) => (
           <FormField
             key={value.value}
@@ -24,14 +61,43 @@ const SignUpStudent = () => {
             render={({ field }) => (
               <FormItem>
                 <FormLabel>{value.label}</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder={value.placeholder} />
-                </FormControl>
+                <div className="flex gap-2">
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder={value.placeholder}
+                      disabled={value.value === "code" || value.value === "password"}
+                      className="disabled:bg-gray-200"
+                    />
+                  </FormControl>
+                  {value.value === "code" && (
+                    <Button
+                      type="button"
+                      variant="outline"
+                      onClick={() => {
+                        const newRandomCode = createRandomCode();
+                        form.setValue("code", newRandomCode);
+                        form.setValue("password", newRandomCode);
+                      }}
+                    >
+                      고유번호 재생성
+                    </Button>
+                  )}
+                  {value.value === "name" && (
+                    <Button type="button" variant="outline" disabled>
+                      @student.com
+                    </Button>
+                  )}
+                </div>
+                {value.value === "password" && (
+                  <FormDescription>비밀번호는 고유번호와 동일합니다.</FormDescription>
+                )}
+                <FormMessage />
               </FormItem>
             )}
           />
         ))}
-        <Button>회원가입</Button>
+        <Button type="submit">회원가입</Button>
       </form>
     </Form>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,34 +7,36 @@ import SignUpStudent from "./SignUpStudent";
 
 const SignUpPage = () => {
   return (
-    <Tabs defaultValue="teacher" className="max-w-[400px]">
-      <TabsList className="w-full grid grid-cols-3">
-        <TabsTrigger value="teacher">선생님</TabsTrigger>
-        <TabsTrigger value="student">학생</TabsTrigger>
-        <TabsTrigger value="parents">학부모</TabsTrigger>
-      </TabsList>
-      <TabsContent value="teacher">
-        <Card>
-          <CardHeader>
-            <CardTitle>선생님 회원가입</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <SignUpTeacher />
-          </CardContent>
-        </Card>
-      </TabsContent>
-      <TabsContent value="student">
-        <Card>
-          <CardHeader>
-            <CardTitle>학생 회원가입</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <SignUpStudent />
-          </CardContent>
-        </Card>
-      </TabsContent>
-      <TabsContent value="parents"></TabsContent>
-    </Tabs>
+    <div className="w-full flex justify-center items-center">
+      <Tabs defaultValue="teacher" className="w-full">
+        <TabsList className="w-full grid grid-cols-3">
+          <TabsTrigger value="teacher">선생님</TabsTrigger>
+          <TabsTrigger value="student">학생</TabsTrigger>
+          <TabsTrigger value="parents">학부모</TabsTrigger>
+        </TabsList>
+        <TabsContent value="teacher">
+          <Card>
+            <CardHeader>
+              <CardTitle>선생님 회원가입</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <SignUpTeacher />
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="student">
+          <Card>
+            <CardHeader>
+              <CardTitle>학생 회원가입</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <SignUpStudent />
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="parents"></TabsContent>
+      </Tabs>
+    </div>
   );
 };
 
