382edb045c7ffd545c39c1d49050b20ddef6d157
Gaoridang <oingc@naver.com>
2024-02-27 03:17:57 +0900

    Refactor: 새로운 타입에 맞게 코드 수정

--- app/classes/hooks/useSelectSchool.ts
+++ app/classes/hooks/useSelectSchool.ts
@@ -1,19 +1,11 @@
 import { create } from "zustand";
 
 interface SelectSchoolState {
-  school: string;
-  grade: string;
-  classNumber: string;
-  setSchool: (school: string) => void;
-  setGrade: (grade: string) => void;
-  setClassNumber: (classNumber: string) => void;
+  selectedSchool: string;
+  setSelectedSchool: (school: string) => void;
 }
 
 export const useSelectSchool = create<SelectSchoolState>((set) => ({
-  school: "",
-  grade: "",
-  classNumber: "",
-  setSchool: (school) => set({ school }),
-  setGrade: (grade) => set({ grade }),
-  setClassNumber: (classNumber) => set({ classNumber }),
+  selectedSchool: "",
+  setSelectedSchool: (selectedSchool) => set({ selectedSchool }),
 }));
