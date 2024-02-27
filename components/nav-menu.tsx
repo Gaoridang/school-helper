cd2b05e40a37b57edc1779921669c07448a96eb7
Gaoridang <oingc@naver.com>
2024-02-27 14:56:49 +0900

    Refactor: 메인, 네비바 로직 수정

--- components/nav-menu.tsx
+++ components/nav-menu.tsx
@@ -14,28 +14,39 @@ import {
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import useSupabaseBrowser from "@/app/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { getRoleBadge } from "@/app/signup/utils/getRoleBadge";
+import { useToast } from "./ui/use-toast";
 
-const NavMenus = () => {
+const NavMenus = ({ user }: { user: User | null }) => {
   const router = useRouter();
+  const supabase = useSupabaseBrowser();
+  const { toast } = useToast();
+
   const signOut = async () => {
-    const supabase = useSupabaseBrowser();
     const { error } = await supabase.auth.signOut();
 
     if (error) {
-      router.push("/error");
+      return toast({
+        title: "로그아웃에 실패했습니다.",
+        description: "다시 시도해주세요.",
+      });
     }
 
-    router.push("/");
+    localStorage.removeItem("user");
+
+    router.push("/signin");
     router.refresh();
   };
 
-  // if (!data)
-  //   return (
-  //     <div className="flex items-center gap-4 py-2">
-  //       <Link href="/signin">로그인</Link>
-  //       <Link href="/signup">회원가입</Link>
-  //     </div>
-  //   );
+  if (!user) {
+    return (
+      <div className="flex items-center gap-4 py-2">
+        <Link href="/signin">로그인</Link>
+        <Link href="/signup">회원가입</Link>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -49,7 +60,9 @@ const NavMenus = () => {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuLabel></DropdownMenuLabel>
+          <DropdownMenuLabel>
+            {user.user_metadata.name} {getRoleBadge(user.user_metadata.role)}
+          </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
             <DropdownMenuItem>
