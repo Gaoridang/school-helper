d8ea71eb91df8b4f544f6a42338cef5057077867
Gaoridang <oingc@naver.com>
2024-02-28 16:30:09 +0900

    Add: 학생 정보 가져오기

--- app/students/[id]/page.tsx
+++ app/students/[id]/page.tsx
@@ -1,7 +1,26 @@
+import useSupabaseServer from "@/app/utils/supabase/server";
+import { cookies } from "next/headers";
 import React from "react";
 
-const StudentDetailPage = () => {
-  return <div>StudentDetailPage</div>;
+interface Props {
+  params: { id: string };
+}
+
+const StudentDetailPage = async ({ params }: Props) => {
+  const cookieStore = cookies();
+  const supabase = useSupabaseServer(cookieStore);
+
+  const { data: student, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("id", params.id)
+    .single();
+
+  return (
+    <div>
+      <pre>{JSON.stringify(student, null, 2)}</pre>
+    </div>
+  );
 };
 
 export default StudentDetailPage;
