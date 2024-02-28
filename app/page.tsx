98a8b22662b8fc468bb14267157383208df30984
Gaoridang <oingc@naver.com>
2024-02-28 13:39:44 +0900

    Add: 메인 페이지 권한에 따른 UI 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import { cookies } from "next/headers";
+import useSupabaseServer from "./utils/supabase/server";
+import TeacherMainPage from "./teacher/page";
+import StudentMainPage from "./student/page";
+
 export default async function Home() {
-  return <></>;
+  const cookieStore = cookies();
+  const supabase = useSupabaseServer(cookieStore);
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return user?.user_metadata.role === "teacher" ? <TeacherMainPage /> : <StudentMainPage />;
 }
