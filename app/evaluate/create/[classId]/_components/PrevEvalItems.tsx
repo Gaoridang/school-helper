ab3b5f8917a4da3695866ea86c9e40444b4144bf
Lee JaeJun <monooi@naver.com>
2024-02-29 13:41:09 +0900

    Add: 이전 항목 불러오기 기능 추가

---
+++ app/evaluate/create/[classId]/_components/PrevEvalItems.tsx
@@ -0,0 +1,104 @@
+import { getEvalItemsByCreatorId } from "@/app/queries/getEvalItemsByCreatorId";
+import { Tables } from "@/app/types/schema";
+import useSupabaseBrowser from "@/app/utils/supabase/client";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { User } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+interface Props {
+  user: User;
+  handleSelectedItems: (selectedItems: string[]) => void;
+}
+
+const PrevEvalItems = ({ user, handleSelectedItems }: Props) => {
+  const [evalItems, setEvalItems] = useState<Tables<"evaluation_items">[]>([]);
+  const [value, setValue] = useState("");
+  const [selectedItemsContent, setSelectedItemsContent] = useState<string[]>([]);
+  const supabase = useSupabaseBrowser();
+  useEffect(() => {
+    const fetchEvalItems = async () => {
+      const { data, error } = await supabase
+        .from("evaluation_items")
+        .select("*")
+        .eq("creator_id", user.id);
+
+      if (!error) {
+        setEvalItems(data);
+      }
+    };
+    fetchEvalItems();
+  }, [supabase, user.id]);
+
+  const filteredItems = evalItems.filter((item) => item.content.includes(value));
+
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="link" type="button" className="px-1 text-indigo-600">
+            이전 항목 가져오기
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="min-h-[400px] flex flex-col">
+          <DialogHeader>
+            <DialogTitle>이전 평가 항목 가져오기</DialogTitle>
+            <DialogDescription>이전에 작성한 평가 항목을 불러올 수 있어요.</DialogDescription>
+          </DialogHeader>
+          <Input
+            placeholder="검색어를 입력하세요."
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <ScrollArea className="max-h-[300px]">
+            {filteredItems.map((item) => (
+              <div key={item.id} className="flex items-center space-x-2 mt-2">
+                <Checkbox
+                  id={item.id.toString()}
+                  onCheckedChange={(checked) => {
+                    return checked
+                      ? setSelectedItemsContent([...selectedItemsContent, item.content])
+                      : setSelectedItemsContent(
+                          selectedItemsContent.filter((content) => content !== item.content),
+                        );
+                  }}
+                />
+                <label htmlFor={item.id.toString()}>{item.content}</label>
+              </div>
+            ))}
+          </ScrollArea>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant="outline">
+                취소
+              </Button>
+            </DialogClose>
+            <Button
+              type="button"
+              onClick={() => {
+                handleSelectedItems(selectedItemsContent);
+                setSelectedItemsContent([]);
+              }}
+            >
+              불러오기
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default PrevEvalItems;
