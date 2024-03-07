0534e39499c326e39ba3387cb2dc202fc43f49c9
Gaoridang <oingc@naver.com>
2024-03-08 00:34:53 +0900

    Add: 네비바 로고 추가

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,13 @@
 import NavMenus from "@/components/nav-menu";
+import { CheckCircle } from "lucide-react";
 import Link from "next/link";
 
 export async function Navbar() {
   return (
     <div className="flex items-center justify-between gap-4 px-5 py-2 border-b">
-      <Link href="/">HOME</Link>
+      <Link href="/" className="flex items-center gap-2 font-bold">
+        <CheckCircle strokeWidth={3} className="w-5 h-5 text-indigo-500" /> CHECKMATE
+      </Link>
       <NavMenus />
     </div>
   );
