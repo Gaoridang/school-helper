1785b2009554542b624b25a9931bc6e8a487157a
Gaoridang <oingc@naver.com>
2024-03-31 22:57:31 +0900

    Fix: 학년 수 6학년까지로 수정

--- app/(teacher)/classes/components/SelectGrade.tsx
+++ app/(teacher)/classes/components/SelectGrade.tsx
@@ -10,12 +10,6 @@ const data = [
   { label: "4학년", value: "4" },
   { label: "5학년", value: "5" },
   { label: "6학년", value: "6" },
-  { label: "7학년", value: "7" },
-  { label: "8학년", value: "8" },
-  { label: "9학년", value: "9" },
-  { label: "10학년", value: "10" },
-  { label: "11학년", value: "11" },
-  { label: "12학년", value: "12" },
 ];
 
 const SelectGrade = ({
