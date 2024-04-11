5ae332b947b69dd58e1a5f5d28a1e616f90a12fb
Lee JaeJun <monooi@naver.com>
2024-04-11 23:12:42 +0900

    Refactor: 사이드바 반응형으로 변경

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,25 +2,25 @@ import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
-import { ReactQueryClientProvider } from "./QueryClientProvider";
+import { ReactQueryClientProvider } from "../QueryClientProvider";
+import "../globals.css";
 
 const pretendard = localFont({
   src: [
     {
-      path: "../public/fonts/SpoqaHanSansNeo-Light.ttf",
+      path: "../../public/fonts/SpoqaHanSansNeo-Light.ttf",
       weight: "300",
     },
     {
-      path: "../public/fonts/SpoqaHanSansNeo-Regular.ttf",
+      path: "../../public/fonts/SpoqaHanSansNeo-Regular.ttf",
       weight: "400",
     },
     {
-      path: "../public/fonts/SpoqaHanSansNeo-Medium.ttf",
+      path: "../../public/fonts/SpoqaHanSansNeo-Medium.ttf",
       weight: "500",
     },
     {
-      path: "../public/fonts/SpoqaHanSansNeo-Bold.ttf",
+      path: "../../public/fonts/SpoqaHanSansNeo-Bold.ttf",
       weight: "700",
     },
   ],
