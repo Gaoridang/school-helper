5d32ea1ae358831a7eaa746872f7c97e6abb0c8c
Lee JaeJun <monooi@naver.com>
2024-04-11 23:18:21 +0900

    Refactor: Auth 관련 페이지 layout 제거

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -3,7 +3,7 @@ import SignInForm from "./SignInForm";
 
 const SignInPage = () => {
   return (
-    <div className="max-w-md m-auto mt-5">
+    <div className="p-4 max-w-xl m-auto flex flex-col">
       <PageTitle title="로그인" description="로그인하고 다양한 기능을 이용해보세요 ⭐️" />
       <SignInForm />
     </div>
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,8 +1,10 @@
+import PageTitle from "@/app/components/PageTitle";
 import SignUpForm from "./SignUpForm";
 
 const SignUpPage = () => {
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="p-4 max-w-xl m-auto flex flex-col">
+      <PageTitle title="환영합니다!" description="효율적인 학생 관리 도구를 사용해보세요 ✨" />
       <SignUpForm />
     </div>
   );
