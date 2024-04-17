4c18d6c805bc73d84000449138295dcb60eac3f9
Gaoridang <oingc@naver.com>
2024-04-17 18:09:07 +0900

    Remove: 홈 레이아웃 폰트 적용 방식 변경

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,34 +1,11 @@
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "../globals.css";
 import { ReactQueryClientProvider } from "../QueryClientProvider";
 import Sidebar from "../Sidebar";
 import SidebarItem from "../components/sidebar/SidebarItem";
 import { LayoutDashboard, MessageCircle, Settings } from "lucide-react";
-
-const pretendard = localFont({
-  src: [
-    {
-      path: "../../public/fonts/SpoqaHanSansNeo-Light.ttf",
-      weight: "300",
-    },
-    {
-      path: "../../public/fonts/SpoqaHanSansNeo-Regular.ttf",
-      weight: "400",
-    },
-    {
-      path: "../../public/fonts/SpoqaHanSansNeo-Medium.ttf",
-      weight: "500",
-    },
-    {
-      path: "../../public/fonts/SpoqaHanSansNeo-Bold.ttf",
-      weight: "700",
-    },
-  ],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -43,7 +20,7 @@ export default function RootLayout({
   return (
     <ReactQueryClientProvider>
       <html lang="en" suppressHydrationWarning className="h-full">
-        <body className={cn(pretendard.className, "relative flex h-full")}>
+        <body className={cn("font-spoqa relative flex h-full")}>
           <Sidebar>
             <SidebarItem
               icon={<LayoutDashboard size={22} strokeWidth={2.5} />}
