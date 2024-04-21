a94a2f520245cc01e8b1b6f9a1858d4224ecd78d
Gaoridang <oingc@naver.com>
2024-04-21 18:36:44 +0900

    Refactor: 미들웨어 수정

--- middleware.ts
+++ middleware.ts
@@ -1,81 +1,8 @@
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
+import { updateSession } from "./app/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value: "",
-            ...options,
-          });
-        },
-      },
-    },
-  );
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  // if user is not teacher, redirect to should-be-teacher page
-  if (request.nextUrl.pathname.startsWith("/classes/create")) {
-    if (user?.user_metadata.role !== "teacher") {
-      return NextResponse.redirect(new URL("/should-be-teacher", request.url));
-    }
-  }
-
-  // if user is not signed in, redirect to sign in page
-  if (!request.nextUrl.pathname.startsWith("/sign") && !user) {
-    return NextResponse.redirect(new URL("/signin", request.url));
-  }
-
-  // if user is signed in, redirect to home page
-  if (request.nextUrl.pathname.startsWith("/sign") && user) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  return response;
+  return await updateSession(request);
 }
 
 export const config = {
