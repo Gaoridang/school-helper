627f635db720cd26d075e44ea15283798823117a
Lee JaeJun <monooi@naver.com>
2024-05-06 17:28:00 +0900

    Refactor: 프로필에 학생 코드 추가

--- app/(home)/settings/page.tsx
+++ app/(home)/settings/page.tsx
@@ -11,6 +11,8 @@ const SettingsPage = async () => {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { data } = await supabase.from("users").select("student_code").eq("id", user?.id!).single();
+
   const classList = await fetchClassListByUserId(user!.id);
   const selectedClass = classList.find((c) => c.is_primary);
   const selectedStudentList = await fetchLinkedStudent(user?.id!, selectedClass?.class_id!);
@@ -21,7 +23,12 @@ const SettingsPage = async () => {
       {/* 이름 */}
       {/* 학급 */}
       {/* 학부모: 학생 / 학생: 고유번호 / 선생님: 학급코드 */}
-      <SettingsForm user={user!} classList={classList} selectedStudentList={selectedStudentList} />
+      <SettingsForm
+        user={user!}
+        classList={classList}
+        selectedStudentList={selectedStudentList}
+        studentCode={data?.student_code}
+      />
     </div>
   );
 };
